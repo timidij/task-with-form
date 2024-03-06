554de6469a0c43ff1d9157fdee5febe1563021e6
Odubo Timidi James <81032342+timidij@users.noreply.github.com>
2024-03-06 08:22:33 +0100

    Add files via upload

---
+++ designforme.js
@@ -0,0 +1,19 @@
+document.addEventListener("Domcontent loaded",function (){
+document.querySelector("#nameform").onsubmit=function(){
+const heading=document.querySelector('hi')
+const firstname=document.querySelector('#Fname').value
+const lastname=document.querySelector('#Lname').value
+const middlename=document.querySelector('#Mname').value
+const test1s=document.querySelector('#test 1').value
+const test2=document.querySelector('#test 2').value
+const Exam=document.querySelector('#exm').value
+const Matricno=document.querySelector('#mat no').value
+
+console.log(firstname)
+
+}
+})
+
+
+
+console.log("hello")
